1b98ba4d58193773909aaf3b92f2912cd073f373
Michael M. Tiller <michael.tiller@juliahub.com>
2023-12-30 10:42:50 -0500

    feat: added ability to roll dice

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import { useState } from "react";
+import random from "random";
+import {
+  IconDice1,
+  IconDice2,
+  IconDice3,
+  IconDice4,
+  IconDice5,
+  IconDice6,
+} from "@tabler/icons-react";
 
 export interface State {
+  lastRoll: [number, number] | null;
   top: number[];
   topTaken: boolean[];
   bottom: number[];
@@ -9,11 +19,34 @@ export interface State {
 }
 
 const initialState: State = {
+  lastRoll: null,
   top: [1, 2, 3, 4, 5, 6, 7, 8, 9],
   topTaken: [false, true, false, false, false, false, false, false, false],
   bottom: [9, 8, 7, 6, 5, 4, 3, 2, 1],
   bottomTaken: [false, false, false, false, false, false, false, false, false],
   count: 0,
+};
+
+export interface DieProps {
+  value: number;
+}
+
+export const Die = (props: DieProps) => {
+  switch (props.value) {
+    case 1:
+      return <IconDice1 />;
+    case 2:
+      return <IconDice2 />;
+    case 3:
+      return <IconDice3 />;
+    case 4:
+      return <IconDice4 />;
+    case 5:
+      return <IconDice5 />;
+    case 6:
+      return <IconDice6 />;
+  }
+  return null;
 };
 
 export interface NumberButtonProps {
@@ -39,23 +72,43 @@ export const NumberButton = (props: NumberButtonProps) => {
 };
 
 function App() {
-  const [state] = useState(initialState);
+  const [state, setState] = useState(initialState);
 
   return (
     <div>
       <div>
         <h1>Game</h1>
-        <div>
-          <div style={{ display: "flex" }}>
+        {state.lastRoll == null ? (
+          <h2>Start by rolling the Die</h2>
+        ) : (
+          <h2>
+            Last Roll: <Die value={state.lastRoll[0]} /> +{" "}
+            <Die value={state.lastRoll[1]} />={" "}
+            {state.lastRoll[0] + state.lastRoll[1]}
+          </h2>
+        )}
+        <div id="numbers">
+          <div id="top_numbers" style={{ display: "flex" }}>
             {state.top.map((value, index) => (
               <NumberButton value={value} taken={state.topTaken[index]} />
             ))}
           </div>
-          <div style={{ display: "flex" }}>
+          <div id="bottom_numbers" style={{ display: "flex" }}>
             {state.bottom.map((value, index) => (
               <NumberButton value={value} taken={state.bottomTaken[index]} />
             ))}
           </div>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              const roll1 = random.int(1, 6);
+              const roll2 = random.int(1, 6);
+              setState((s) => ({ ...s, lastRoll: [roll1, roll2] }));
+            }}
+          >
+            Roll Die
+          </button>
         </div>
       </div>
       <div style={{ width: "100%", padding: "2em", border: "3px solid red" }}>
