5a20a52d8b222fe5c39a0a26974dd60852cd092c
Michael M. Tiller <michael.tiller@juliahub.com>
2023-12-30 11:54:57 -0500

    feat: playable version of game
    Doesn't detect when you have lost.

--- src/App.tsx
+++ src/App.tsx
@@ -23,23 +23,105 @@ function selectKey(s: Selected): string {
   return `${s.row}-${s.col}`;
 }
 
+function parseKey(s: string): Selected {
+  if (s.startsWith("top-")) {
+    return {
+      row: "top",
+      col: +s.slice(4),
+    };
+  } else if (s.startsWith("bottom-")) {
+    return {
+      row: "bottom",
+      col: +s.slice(7),
+    };
+  }
+  throw new Error(`The key ${s} is not a valid select key`);
+}
+
 const initialState: State = {
   lastRoll: null,
   readyToRoll: true,
   selected: [],
   top: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  topTaken: [false, true, false, false, false, false, false, false, false],
+  topTaken: [false, false, false, false, false, false, false, false, false],
   bottom: [9, 8, 7, 6, 5, 4, 3, 2, 1],
   bottomTaken: [false, false, false, false, false, false, false, false, false],
   count: 0,
 };
 
-function canSelect(): boolean {
+function canSelect(me: Selected, state: State): boolean {
+  if (me.row === "top") {
+    if (state.topTaken[me.col]) {
+      return false;
+    }
+    if (state.bottomTaken[me.col] == false) {
+      return false;
+    }
+  } else {
+    if (state.bottomTaken[me.col]) {
+      return false;
+    }
+  }
   return true;
 }
 
 function App() {
   const [state, setState] = useState(initialState);
+
+  const toggleHandler = (me: Selected) => {
+    console.log("Clicked on ", me);
+    if (canSelect(me, state)) {
+      const key = selectKey(me);
+      let nextSelected = [...state.selected];
+      const myIndex = nextSelected.indexOf(key);
+      if (myIndex == -1) {
+        nextSelected = [...state.selected, key];
+      } else {
+        nextSelected = [
+          ...nextSelected.slice(0, myIndex),
+          ...nextSelected.slice(myIndex + 1),
+        ];
+      }
+      if (state.lastRoll != null) {
+        const dieSum = state.lastRoll[0] + state.lastRoll[1];
+        const selectedSum = nextSelected.reduce((subtotal, cur) => {
+          const me = parseKey(cur);
+          if (me.row == "top") {
+            return subtotal + state.top[me.col];
+          } else {
+            return subtotal + state.bottom[me.col];
+          }
+        }, 0);
+        console.log("Sum of dice = ", dieSum);
+        console.log("Sum of selected = ", selectedSum);
+        if (dieSum == selectedSum) {
+          const nextState = { ...state };
+          for (const key of nextSelected) {
+            const me = parseKey(key);
+            // Mark selected item as taken
+            if (me.row == "top") {
+              const newTaken = [...nextState.topTaken];
+              newTaken[me.col] = true;
+              nextState.topTaken = newTaken;
+            } else {
+              const newTaken = [...nextState.bottomTaken];
+              newTaken[me.col] = true;
+              nextState.bottomTaken = newTaken;
+            }
+          }
+          nextState.selected = [];
+          nextState.readyToRoll = true;
+          setState(nextState);
+          return;
+        }
+      }
+      // Reset selected to be empty
+      setState((s) => ({
+        ...s,
+        selected: nextSelected,
+      }));
+    }
+  };
 
   return (
     <div>
@@ -60,40 +142,30 @@ function App() {
               const me: Selected = { row: "top", col: index };
               return (
                 <NumberButton
-                  key={`top-${index}`}
+                  key={selectKey(me)}
                   value={value}
                   taken={state.topTaken[index]}
                   selected={state.selected.indexOf(selectKey(me)) != -1}
-                  toggle={() => {
-                    console.log("Clicked on ", me);
-                    if (canSelect()) {
-                      const key = selectKey(me);
-                      console.log("Previously selected", state.selected);
-                      let nextSelected = [...state.selected];
-                      if (nextSelected.indexOf(key) == -1) {
-                        nextSelected = [...state.selected, key];
-                      }
-                      console.log("Next Selected should be", nextSelected);
-                      setState((s) => ({
-                        ...s,
-                        selected: nextSelected,
-                      }));
-                    }
-                  }}
+                  toggle={() => toggleHandler(me)}
                 />
               );
             })}
           </div>
           <div id="bottom_numbers" style={{ display: "flex" }}>
-            {state.bottom.map((value, index) => (
-              <NumberButton
-                key={`bottom-${index}`}
-                value={value}
-                taken={state.bottomTaken[index]}
-                selected={false}
-                toggle={() => {}}
-              />
-            ))}
+            {state.bottom.map((value, index) => {
+              const me: Selected = { row: "bottom", col: index };
+              return (
+                <NumberButton
+                  key={selectKey(me)}
+                  value={value}
+                  taken={state.bottomTaken[index]}
+                  selected={state.selected.indexOf(selectKey(me)) != -1}
+                  toggle={() => {
+                    toggleHandler(me);
+                  }}
+                />
+              );
+            })}
           </div>
         </div>
         <div>
@@ -111,12 +183,13 @@ function App() {
           >
             Roll Die
           </button>
+          <button onClick={() => setState(initialState)}>Reset</button>
         </div>
       </div>
-      <div style={{ width: "100%", padding: "2em", border: "3px solid red" }}>
+      {/* <div style={{ width: "100%", padding: "2em", border: "3px solid red" }}>
         <h1>State:</h1>
         <pre>{JSON.stringify(state, null, 4)}</pre>
-      </div>
+      </div> */}
     </div>
   );
 }
